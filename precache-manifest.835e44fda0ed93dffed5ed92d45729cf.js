a7e9d2c0949446cffc59c83c6314c59ece46458a
Mykhailo Fedorko <m.fedorko@sedna.software>
2020-03-12 10:42:47 +0200

    Updates

---
+++ precache-manifest.835e44fda0ed93dffed5ed92d45729cf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b11308210434656d636af0b65c98512",
+    "url": "/react-full-calendar-JoinToIT/index.html"
+  },
+  {
+    "revision": "508038376ac5346b5fa4",
+    "url": "/react-full-calendar-JoinToIT/static/css/2.26061d2a.chunk.css"
+  },
+  {
+    "revision": "d7ce7981d5ad1c563999",
+    "url": "/react-full-calendar-JoinToIT/static/css/main.d9d3d5ce.chunk.css"
+  },
+  {
+    "revision": "508038376ac5346b5fa4",
+    "url": "/react-full-calendar-JoinToIT/static/js/2.89d6e70b.chunk.js"
+  },
+  {
+    "revision": "55b44eb4dc26e3f7c265a507d94a7aa3",
+    "url": "/react-full-calendar-JoinToIT/static/js/2.89d6e70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ce7981d5ad1c563999",
+    "url": "/react-full-calendar-JoinToIT/static/js/main.5378367f.chunk.js"
+  },
+  {
+    "revision": "0f2addbd1e0a7aa1d52e",
+    "url": "/react-full-calendar-JoinToIT/static/js/runtime-main.51ef19b2.js"
+  },
+  {
+    "revision": "92ccddf2ecba3ff5754ba8f2ebd8b1aa",
+    "url": "/react-full-calendar-JoinToIT/static/media/calendar.92ccddf2.svg"
+  },
+  {
+    "revision": "06412bd8ae8f9110994a56e33b4f8c85",
+    "url": "/react-full-calendar-JoinToIT/static/media/clock.06412bd8.svg"
+  },
+  {
+    "revision": "798fcea534f595bc5ced119433778e9b",
+    "url": "/react-full-calendar-JoinToIT/static/media/color-picker.798fcea5.svg"
+  },
+  {
+    "revision": "a7a1e1c22db3a97eea0a3d9e9d89dca2",
+    "url": "/react-full-calendar-JoinToIT/static/media/name.a7a1e1c2.svg"
+  }
+]);
